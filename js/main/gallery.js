ab65bd165c31408f3628109ca92d49352fc38451
leejungeun <jungeun623@gmail.com>
2024-10-31 17:55:40 +0900

    세일 상품 페이지 수정, 장바구니 js 수정

--- js/main/gallery.js
+++ js/main/gallery.js
@@ -1,8 +1,16 @@
-// js
 window.onload = function () {
+  document.getElementById("bag-link").addEventListener("click", function (event) {
+    // 'userFind' 항목이 localStorage에 있는지 확인
+    const userFind = JSON.parse(localStorage.getItem("userFind")) || {};
 
-}
-// jquery
-$(document).ready(function () {
-
-})
+    if (userFind.username) {
+      // 로그인 상태일 경우 bagitem.html로 이동
+      window.location.href = "bagitem.html";
+    } else {
+      // 로그아웃 상태일 경우 경고 메시지 및 로그인 페이지로 이동
+      event.preventDefault();
+      alert("로그인하세요");
+      window.location.href = "login.html";
+    }
+  });
+};
